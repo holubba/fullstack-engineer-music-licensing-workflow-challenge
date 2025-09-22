9bac98ab1aa0e9372ee588a3bcaf4a28ac47a9b2
holubba <zld3707@gmail.com>
2025-09-22 14:57:35 +0200

    fix: added entities to get entities method and ditched base entity idea

--- src/app/setup/get-entities.ts
+++ src/app/setup/get-entities.ts
@@ -1,5 +1,12 @@
-import { Movies } from '../database/entities'
+import {
+  LicenseHistory,
+  Licenses,
+  Movies,
+  Scenes,
+  Tracks,
+  Songs,
+} from '../database/entities'
 
 export const getEntities = () => {
-  return [Movies]
+  return [Movies, Scenes, Licenses, LicenseHistory, Songs, Tracks]
 }
